0f370d3f02b5aa9876c6098103038a9c069bddb3
Saiberion <saiberion@web.de>
2018-12-04 19:37:23 +0100

    Solved Day 1 part 2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -19,10 +19,36 @@ namespace Day01
             return result;
         }
 
+        static int FrequencyShiftingFirstDuplicate(List<string> numberList)
+        {
+            int result = 0;
+            bool foundDuplicate = false;
+            List<int> previousFrequencies = new List<int>();
+            previousFrequencies.Add(result);
+            while (!foundDuplicate)
+            {
+                foreach (string s in numberList)
+                {
+                    result += int.Parse(s);
+                    if (previousFrequencies.Contains(result))
+                    {
+                        foundDuplicate = true;
+                        break;
+                    }
+                    else
+                    {
+                        previousFrequencies.Add(result);
+                    }
+                }
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
             List<string> input = InputLoader.LoadByLines("input.txt");
             Console.WriteLine(string.Format("Final frequency: {0}", FrequencyShifting(input)));
+            Console.WriteLine(string.Format("First duplicate frequency: {0}", FrequencyShiftingFirstDuplicate(input)));
             Console.ReadLine();
         }
     }
