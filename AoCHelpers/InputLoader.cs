d1a7f6d4c3d0f6b7c269581f7347f9dd8462f7b3
Saiberion <saiberion@web.de>
2018-12-04 18:56:46 +0100

    Solved Day 1 Part 1

---
+++ AoCHelpers/InputLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCHelpers
+{
+    public class InputLoader
+    {
+        public static List<string> LoadByLines(string filename)
+        {
+            StreamReader file = new StreamReader(filename);
+            string line;
+            List<string> lines = new List<string>();
+
+            while ((line = file.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            file.Close();
+            return lines;
+        }
+    }
+}
--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AoCHelpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,21 @@ namespace Day01
 {
     class Program
     {
+        static int FrequencyShifting(List<string> numberList)
+        {
+            int result = 0;
+            foreach(string s in numberList)
+            {
+                result += int.Parse(s);
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo Welt");
+            List<string> input = InputLoader.LoadByLines("input.txt");
+            Console.WriteLine(string.Format("Final frequency: {0}", FrequencyShifting(input)));
+            Console.ReadLine();
         }
     }
 }
