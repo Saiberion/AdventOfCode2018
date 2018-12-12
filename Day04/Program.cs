a02ea2f9b343d4d05eebdda19d458bb9e2fe3c21
Saiberion <saiberion@web.de>
2018-12-12 11:08:04 +0100

    Day 3 changed target framework Day 4 completely solved

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -78,22 +78,114 @@ namespace Day04
             return sortedGuardLog;
         }
 
-        static Dictionary<int, int> GetGuardsSleepTime(List<GuardLog> log)
+        static Dictionary<int, int[]> CreateGuardSleepChart(List<GuardLog> log)
         {
-            Dictionary<int, int> dict = new Dictionary<int, int>();
+            Dictionary<int, int[]> dict = new Dictionary<int, int[]>();
 
+            int sleepStart = 0;
+            int sleepEnd = 0;
+            int currentGuard = 0;
+            foreach (GuardLog gl in log)
+            {
+                switch (gl.GuardAction)
+                {
+                    case EGuardAction.eGuardActionBeginsShift:
+                        currentGuard = gl.GuardID;
+                        if (!dict.Keys.Contains(currentGuard))
+                        {
+                            dict.Add(currentGuard, new int[60]);
+                        }
+                        break;
+                    case EGuardAction.eGuardActionFallsAsleep:
+                        sleepStart = gl.TimeStamp.Minute;
+                        break;
+                    case EGuardAction.eGuardActionWakesUp:
+                        sleepEnd = gl.TimeStamp.Minute;
 
+                        for (int i = sleepStart; i < sleepEnd; i++)
+                        {
+                            dict[currentGuard][i]++;
+                        }
+                        break;
+                }
+            }
 
-            return new Dictionary<int, int>();
+            return dict;
+        }
+
+        static int[] GetMostAsleepStrat1(Dictionary<int, int[]> sleepChart)
+        {
+            Dictionary<int, int> sleepTime = new Dictionary<int, int>();
+
+            foreach(int g in sleepChart.Keys)
+            {
+                sleepTime.Add(g, 0);
+                for (int i = 0; i < sleepChart[g].Length; i++)
+                {
+                    sleepTime[g] += sleepChart[g][i];
+                }
+            }
+
+            int sleptMinutes = 0;
+            int sleepyGuard = 0;
+
+            foreach (KeyValuePair<int, int> kvp in sleepTime)
+            {
+                if (kvp.Value > sleptMinutes)
+                {
+                    sleepyGuard = kvp.Key;
+                    sleptMinutes = kvp.Value;
+                }
+            }
+
+            int sleepyMinute = 0;
+            int sleepyMinuteTime = 0;
+            for (int i = 0; i < sleepChart[sleepyGuard].Length; i++)
+            {
+                if (sleepChart[sleepyGuard][i] > sleepyMinuteTime)
+                {
+                    sleepyMinuteTime = sleepChart[sleepyGuard][i];
+                    sleepyMinute = i;
+                }
+            }
+
+            return new int[] { sleepyGuard, sleepyMinute };
+        }
+
+        static int[] GetMostAsleepStrat2(Dictionary<int, int[]> sleepChart)
+        {
+            //Dictionary<int, int> sleepTime = new Dictionary<int, int>();
+
+            int sleepyMinuteTime = -1;
+            int sleepyMinute = -1;
+            int sleepyGuard = -1;
+            foreach (int g in sleepChart.Keys)
+            {
+                for (int i = 0; i < sleepChart[g].Length; i++)
+                {
+                    if (sleepChart[g][i] > sleepyMinuteTime)
+                    {
+                        sleepyMinuteTime = sleepChart[g][i];
+                        sleepyMinute = i;
+                        sleepyGuard = g;
+                    }
+                }
+            }
+
+            return new int[] { sleepyGuard, sleepyMinute };
         }
 
         static void Main(string[] args)
         {
-            List<string> input = InputLoader.LoadByLines("example.txt");
+            List<string> input = InputLoader.LoadByLines("input.txt");
             List<GuardLog> guardLog = FillGuardLog(input);
-            Dictionary<int, int> guardSleepTime = GetGuardsSleepTime(guardLog);
-            //Console.WriteLine(string.Format("overlapping area: {0}", GetOverlappingArea(input)));
-            //Console.WriteLine(string.Format("unique claim {0}", findUniqueClaim(input)));
+            Dictionary<int, int[]> sleepChart = CreateGuardSleepChart(guardLog);
+
+            int[] resultStrat1 = GetMostAsleepStrat1(sleepChart);
+            int[] resultStrat2 = GetMostAsleepStrat2(sleepChart);
+
+            Console.WriteLine(string.Format("Strat 1: chosen guard * sleepiest minute = {0}", resultStrat1[0] * resultStrat1[1]));
+            Console.WriteLine(string.Format("strat 2: chosen guard * sleepiest minute = {0}", resultStrat2[0] * resultStrat2[1]));
             Console.ReadLine();
         }
     }
