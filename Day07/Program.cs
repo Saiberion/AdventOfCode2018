edb0b478182eb2b910975cdc3a2e29e6ef0a6a2c
Saiberion <saiberion@web.de>
2018-12-13 18:21:53 +0100

    Day 7 Part 2 solved

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -13,6 +13,20 @@ namespace Day07
         public List<string> PreCondition { get; set; }
     }
 
+    class Worker
+    {
+        public int ID { get; set; }
+        public string WorkingOnInstr { get; set; }
+        public int BusyTime { get; set; }
+
+        public Worker(int id)
+        {
+            this.ID = id;
+            this.WorkingOnInstr = string.Empty;
+            this.BusyTime = 0;
+        }
+    }
+
     class Program
     {
         static Dictionary<string, List<string>> GetInstructionOrder(List<string> input)
@@ -68,13 +82,97 @@ namespace Day07
             return sb.ToString();
         }
 
+        static Worker GetAvailableWorker(List<Worker> worker)
+        {
+            Worker work = null;
+
+            foreach(Worker w in worker)
+            {
+                if (w.WorkingOnInstr.Equals(string.Empty) && (w.BusyTime == 0))
+                {
+                    work = w;
+                    break;
+                }
+            }
+
+            return work;
+        }
+
+        static int GetInstructionTime(Dictionary<string, List<string>> instructionNodes, int baseTime, int workerCount)
+        {
+            int timeCounter = 0;
+            List<Worker> worker = new List<Worker>();
+
+            for (int i = 0; i < workerCount; i++)
+            {
+                worker.Add(new Worker(i));
+            }
+
+            while (true)
+            {
+                List<string> availableInstructions = new List<string>();
+                foreach (KeyValuePair<string, List<string>> kvp in instructionNodes)
+                {
+                    if (kvp.Value.Count == 0)
+                    {
+                        availableInstructions.Add(kvp.Key);
+                    }
+                }
+
+                availableInstructions.Sort();
+                foreach(string instr in availableInstructions)
+                {
+                    Worker availableWorker = GetAvailableWorker(worker);
+                    if (availableWorker != null)
+                    {
+                        availableWorker.WorkingOnInstr = instr;
+                        availableWorker.BusyTime = instr[0] - 'A' + baseTime + 1;
+                        instructionNodes.Remove(availableWorker.WorkingOnInstr);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                timeCounter++;
+                bool allIdle = true;
+                foreach (Worker w in worker)
+                {
+                    if (w.BusyTime > 0)
+                    {
+                        allIdle = false;
+                        w.BusyTime--;
+                    }
+                    
+                    if (!w.WorkingOnInstr.Equals(string.Empty) && (w.BusyTime == 0))
+                    {
+                        foreach (KeyValuePair<string, List<string>> kvp in instructionNodes)
+                        {
+                            kvp.Value.Remove(w.WorkingOnInstr);
+                        }
+                        w.WorkingOnInstr = string.Empty;
+                    }
+                }
+                if (allIdle)
+                {
+                    break;
+                }
+            }
+
+            return --timeCounter;
+        }
+
         static void Main(string[] args)
         {
             List<string> input = InputLoader.LoadByLines("input.txt");
             Dictionary<string, List<string>> instructionNodes = GetInstructionOrder(input);
             string instrOrder = GetInstructionOrder(instructionNodes);
+            instructionNodes = GetInstructionOrder(input);
+            int instrTime = GetInstructionTime(instructionNodes, 60, 5);
 
             Console.WriteLine(string.Format("Instruction order: {0}", instrOrder));
+            Console.WriteLine(string.Format("Instruction time with helpers: {0}", instrTime));
             Console.ReadLine();
         }
     }
