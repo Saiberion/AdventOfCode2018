70af590859cca362b9c3365ed4fa9c47823d2cb8
Saiberion <saiberion@web.de>
2019-07-11 18:24:25 +0200

    ported day 7 to new layout

--- Days/Day07.cs
+++ Days/Day07.cs
@@ -6,45 +6,173 @@ using System.Threading.Tasks;
 
 namespace Days
 {
+    class Worker
+    {
+        public int ID { get; set; }
+        public string WorkingOnInstr { get; set; }
+        public int BusyTime { get; set; }
+
+        public Worker(int id)
+        {
+            this.ID = id;
+            this.WorkingOnInstr = string.Empty;
+            this.BusyTime = 0;
+        }
+    }
+
     public class Day07 : Day
     {
         public Day07()
         {
-            Part1Text = "Final frequency";
-            Part2Text = "First duplicate frequency";
+            Part1Text = "Instruction order";
+            Part2Text = "Instruction time with helpers";
 
             Load("inputs/day07.txt");
         }
 
-        override public void Solve()
+        static Dictionary<string, List<string>> GetInstructionOrder(List<string> input)
         {
-            int result = 0;
-            foreach (string s in Input)
+            Dictionary<string, List<string>> instructionNodes = new Dictionary<string, List<string>>();
+            foreach (string s in input)
             {
-                result += int.Parse(s);
-            }
-            Part1Solution = result.ToString();
+                string[] splitted = s.Split(new char[] { ' ' });
+                string preCond = splitted[1];
+                string instrNode = splitted[7];
 
-            result = 0;
-            bool foundDuplicate = false;
-            HashSet<int> previousFrequencies = new HashSet<int>
-            {
-                result
-            };
-            while (!foundDuplicate)
-            {
-                foreach (string s in Input)
+                if (!instructionNodes.ContainsKey(preCond))
                 {
-                    result += int.Parse(s);
+                    instructionNodes.Add(preCond, new List<string>());
+                }
 
-                    if (!previousFrequencies.Add(result))
+                if (!instructionNodes.ContainsKey(instrNode))
+                {
+                    instructionNodes.Add(instrNode, new List<string>());
+                }
+
+                instructionNodes[instrNode].Add(preCond);
+            }
+
+            return instructionNodes;
+        }
+
+        static string GetInstructionOrder(Dictionary<string, List<string>> instructionNodes)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            while (instructionNodes.Count > 0)
+            {
+                List<string> availableInstructions = new List<string>();
+                foreach (KeyValuePair<string, List<string>> kvp in instructionNodes)
+                {
+                    if (kvp.Value.Count == 0)
                     {
-                        foundDuplicate = true;
+                        availableInstructions.Add(kvp.Key);
+                    }
+                }
+
+                availableInstructions.Sort();
+                sb.Append(availableInstructions[0]);
+                instructionNodes.Remove(availableInstructions[0]);
+
+                foreach (KeyValuePair<string, List<string>> kvp in instructionNodes)
+                {
+                    kvp.Value.Remove(availableInstructions[0]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        static Worker GetAvailableWorker(List<Worker> worker)
+        {
+            Worker work = null;
+
+            foreach (Worker w in worker)
+            {
+                if (w.WorkingOnInstr.Equals(string.Empty) && (w.BusyTime == 0))
+                {
+                    work = w;
+                    break;
+                }
+            }
+
+            return work;
+        }
+
+        static int GetInstructionTime(Dictionary<string, List<string>> instructionNodes, int baseTime, int workerCount)
+        {
+            int timeCounter = 0;
+            List<Worker> worker = new List<Worker>();
+
+            for (int i = 0; i < workerCount; i++)
+            {
+                worker.Add(new Worker(i));
+            }
+
+            while (true)
+            {
+                List<string> availableInstructions = new List<string>();
+                foreach (KeyValuePair<string, List<string>> kvp in instructionNodes)
+                {
+                    if (kvp.Value.Count == 0)
+                    {
+                        availableInstructions.Add(kvp.Key);
+                    }
+                }
+
+                availableInstructions.Sort();
+                foreach (string instr in availableInstructions)
+                {
+                    Worker availableWorker = GetAvailableWorker(worker);
+                    if (availableWorker != null)
+                    {
+                        availableWorker.WorkingOnInstr = instr;
+                        availableWorker.BusyTime = instr[0] - 'A' + baseTime + 1;
+                        instructionNodes.Remove(availableWorker.WorkingOnInstr);
+                    }
+                    else
+                    {
                         break;
                     }
                 }
+
+                timeCounter++;
+                bool allIdle = true;
+                foreach (Worker w in worker)
+                {
+                    if (w.BusyTime > 0)
+                    {
+                        allIdle = false;
+                        w.BusyTime--;
+                    }
+
+                    if (!w.WorkingOnInstr.Equals(string.Empty) && (w.BusyTime == 0))
+                    {
+                        foreach (KeyValuePair<string, List<string>> kvp in instructionNodes)
+                        {
+                            kvp.Value.Remove(w.WorkingOnInstr);
+                        }
+                        w.WorkingOnInstr = string.Empty;
+                    }
+                }
+                if (allIdle)
+                {
+                    break;
+                }
             }
-            Part2Solution = result.ToString();
+
+            return --timeCounter;
+        }
+
+        override public void Solve()
+        {
+            Dictionary<string, List<string>> instructionNodes = GetInstructionOrder(Input);
+            string instrOrder = GetInstructionOrder(instructionNodes);
+            instructionNodes = GetInstructionOrder(Input);
+            int instrTime = GetInstructionTime(instructionNodes, 60, 5);
+
+            Part1Solution = instrOrder;
+            Part2Solution = instrTime.ToString();
         }
     }
 }
